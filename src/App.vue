60619e27071e435b38d6b7ad3bf7cb96e367b8c8
M-erick <muriithierick3758@gmail.com>
2024-06-18 10:34:02 +0300

    installed tailindcss typogrsphy

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,51 @@
 <template>
- <div class="flex w-screen h-screen text-gray-700">
-  <div class="flex flex-col flex-shrink-0 w-64 border-r border-gray-300 bg-gray-100">
-
-
-  </div>
-  <div class="flex flex-col flex-grow">
-    <div class="flex flex-col flex-grow overflow-auto">
-
+  <div class="flex w-screen h-screen text-gray-700">
+    <div class="flex flex-col flex-shrink-0 w-64 border-r border-gray-300 bg-gray-100">
+      <!-- Sidebar content -->
     </div>
-    <editor-content :editor="editor"/>
-
+    <div class="flex flex-col flex-grow">
+      <div class="flex flex-col flex-grow overflow-auto">
+        <EditorContent :editor="editor" />
+      </div>
+    </div>
   </div>
-
- </div>
 </template>
 
 <script>
-import {Editor,EditorContent} from '@tiptap/vue-3';
+import { Editor, EditorContent } from '@tiptap/vue-3';
 import StarterKit from '@tiptap/starter-kit';
 
-export default{
-  name:'App',
-  components:{
+export default {
+  name: 'App',
+  components: {
     EditorContent
   },
-  data(){
+  data() {
     return {
-      editor:null
+      editor: null
     }
   },
   mounted() {
     this.editor = new Editor({
-      content:'',
-      extensions:[
+      content: '',
+      extensions: [
         StarterKit
-      ]
+      ],
+      editorProps: {
+        attributes: {
+          class: "custom-editor-style"
+        }
+      }
     });
   },
   beforeUnmount() {
     this.editor.destroy();
   }
 }
-
 </script>
 
 <style>
-
+.custom-editor-style {
+  @apply prose my-6 mx-auto focus:outline-none;
+}
 </style>

0c3620debbdaae8fcb7d1204bab24c7eb0935389
M-erick <muriithierick3758@gmail.com>
2024-06-18 11:07:08 +0300

    managed to  save data in the indexDB

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,9 @@
       <div class="flex flex-col flex-grow overflow-auto">
         <EditorContent :editor="editor" />
       </div>
+      <div class="h-16 bg-gray-100 border-t border-gray-300 text-right">
+        <button @click="saveNote">Save Notes</button>
+      </div>
     </div>
   </div>
 </template>
@@ -23,13 +26,11 @@ export default {
   data() {
     return {
       editor: null,
-      database:null,
+      database: null,
     }
   },
-  async create(){
-    this.databse = await this.getDatabase();
-
-
+  async created() {
+    this.database = await this.getDatabase();
   },
   mounted() {
     this.editor = new Editor({
@@ -47,24 +48,48 @@ export default {
   beforeUnmount() {
     this.editor.destroy();
   },
-  methods:{
-    async getDatabase(){
-      return new Promise((resolve,reject)=>{
-        let db = window.indexedDB.open("notes");
-        db.onerror = e =>{
+  methods: {
+    async getDatabase() {
+      return new Promise((resolve, reject) => {
+        let db = window.indexedDB.open("notes", 2);
+        db.onerror = e => {
           reject("Error opening the database");
-
         };
-        db.onsuccess = e =>{
-          console.log('db.onsucess',e)
-          resolve(e.target.result)
-
+        db.onsuccess = e => {
+          console.log('db.onsuccess', e);
+          resolve(e.target.result);
         };
-        db.onupgradeneeded = e =>{
-          e.target.result.createObjectStore("notes");
-
+        db.onupgradeneeded = e => {
+          let db = e.target.result;
+          if (db.objectStoreNames.contains("notes")) {
+            db.deleteObjectStore("notes");
+          }
+          db.createObjectStore("notes", { keyPath: "created" });
         };
-      })
+      });
+    },
+    async saveNote() {
+      if (!this.database) {
+        console.error("Database not initialized");
+        return;
+      }
+
+      return new Promise((resolve, reject) => {
+        let transaction = this.database.transaction('notes', 'readwrite');
+        transaction.oncomplete = e => {
+          resolve();
+        };
+        transaction.onerror = e => {
+          reject("Error saving the note");
+        };
+
+        let now = new Date();
+        let store = transaction.objectStore('notes');
+        store.add({
+          content: this.editor.getHTML(),
+          created: now.getTime()
+        });
+      });
     }
   }
 }
